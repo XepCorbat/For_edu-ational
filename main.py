8e2c69ecbbe36dadc9732778df88fb0aa3698f44
Corbat <roman11rr@mail.ru>
2025-04-29 14:20:27 +0300

    7.3
    5

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
+from math import*
 
-counter = 0
-n = int(input())
-for i in range(1, n + 1):
-    if (i ** 2) % 10 in {2, 5, 8}:  # Последняя цифра квадрата i
-        counter += i
-print(counter)
+n  = int(input())
+print(factorial(n))
