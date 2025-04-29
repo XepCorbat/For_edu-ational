0cbef855311c6715fdfc7a6438b7d667835c4441
Corbat <roman11rr@mail.ru>
2025-04-29 19:54:57 +0300

    7.4
    3

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = int(input())
+from logging.config import stopListening
 
-count1 = 1
-count2 = 0
-for i in range(n):
-    count3 = count1
-    count1 = count3 + count2
-    count2 = count3
-    print(count3, end=" ")
+text = input()
+while text != "КОНЕЦ" and text !="конец":
+    print(text)
+    text = input()
+
+
