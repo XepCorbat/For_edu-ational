dddd623a16428cc576a614d7e42bb10dda72e530
Corbat <roman11rr@mail.ru>
2025-04-29 19:33:54 +0300

    7.3
    11

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-counter = 0
-for _ in range(10):
-    n = int(input())
-    if n % 2 == 0:
-        counter += 1
-if counter == 10:
-    print("YES")
-else:
-    print("NO")
+n = int(input())
+
+count1 = 1
+count2 = 0
+for i in range(n):
+    count3 = count1
+    count1 = count3 + count2
+    count2 = count3
+    print(count3, end=" ")
