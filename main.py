dde51d571dbc7345b55277d79a8558ae404cf5cf
Corbat <roman11rr@mail.ru>
2025-04-29 14:03:22 +0300

    7.3
    3

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-total = 0
+import math
+
+harmonic = 0
 n = int(input())
-for _ in range(n):
-    num = int(input())
-    total += num
-print(total)
+for i in range(1, n + 1):
+    harmonic += 1 / i
+result = harmonic - math.log(n)
+print(result)
