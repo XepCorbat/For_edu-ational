139f71fca5e3c2eb2421371b76bbd7617d8f7b66
Corbat <roman11rr@mail.ru>
2025-04-29 13:20:46 +0300

    7.3
    2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-a, b = int(input()), int(input())
-counter =0
-if a <= b:
-    for i in range(a, b+1):
-        if i**3%10 == 4 or i**3%10 == 9:
-            counter += 1
-print(counter)
+total = 0
+n = int(input())
+for _ in range(n):
+    num = int(input())
+    total += num
+print(total)
